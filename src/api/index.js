7822144636bfdb98a261f62b0a373866e8b210ce
Arjun E <arjunrk907@gmail.com>
2023-04-01 21:25:28 +0530

    update endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "https://onus-api.onrender.com";
+export const url = "https://onus-api.onrender.com/api";
 
 const instance = axios.create({
   baseURL: url,
